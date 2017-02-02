47ae3bf7342242676e0f1b97dec27687fd86ff62
Amir SANI <reachme@amirsani.com>
2017-02-02 11:17:47 +0100

    update

---
+++ build/lib/sharpeRratio/__init__.py
@@ -0,0 +1,1 @@
+from sharpeRratio import estimateSNR
--- setup.py
+++ setup.py
@@ -4,18 +4,15 @@ from setuptools import setup, find_packages
 
 import pypandoc
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='sharpeRratio',
-    version='0.01',
+    version='0.05',
     description='A moment-free estimator of the Sharpe (signal-to-noise) ratio.',
     long_description=pypandoc.convert('README.md', 'rst'),
     author='Amir Sani',
     author_email='reachme@amirsani.com',
-    url='https://github.com/amirsani/sharpeRratio',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    url='https://github.com/amirsani/sharpeRratio', 
+    packages=find_packages(exclude=('tests', 'docs')),
+    data_files = [("", ["LICENSE"])]
 )
 
