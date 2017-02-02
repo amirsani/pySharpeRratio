c22cb71e01a2fb33f65adebc047f899a111f7cd7
Amir SANI <reachme@amirsani.com>
2017-02-02 12:04:38 +0100

    update

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@
 
 from setuptools import setup, find_packages
 
-import pypandoc
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 
 setup(
     name='pysharperratio',
     version='0.01.01',
     description='A moment-free estimator of the Sharpe (signal-to-noise) ratio.',
-    long_description=pypandoc.convert('README.md', 'rst'),
+    long_description=readme(),
     author='Amir Sani',
     author_email='reachme@amirsani.com',
     url='https://github.com/amirsani/pySharpeRratio', 
-    packages=find_packages(exclude=('tests', 'docs')),
     data_files = [("", ["LICENSE"])]
 )
 
