b72713f83b4dce253d8e97cacb15e82f95a9a866
Amir SANI <reachme@amirsani.com>
2017-02-02 11:03:43 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import pypandoc
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sharpeRratio',
+    version='0.01',
+    description='A moment-free estimator of the Sharpe (signal-to-noise) ratio.',
+    long_description=pypandoc.convert('README.md', 'rst'),
+    author='Amir Sani',
+    author_email='reachme@amirsani.com',
+    url='https://github.com/kennethreitz/samplemod',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
