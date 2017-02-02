3dd8d274afefde6c6ea2ac43a1c6cc20dc9e7b6e
Amir SANI <reachme@amirsani.com>
2017-02-02 12:22:18 +0100

    update

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(
     version='0.01.04',
     description='A moment-free estimator of the Sharpe (signal-to-noise) ratio.',
     author='Amir Sani',
+    url='http://www.amirsani.com',
     author_email='reachme@amirsani.com',
-    url='https://github.com/amirsani/pySharpeRratio' 
-
+    download_url='https://github.com/amirsani/pySharpeRratio',
+    packages=['pysharperratio'],
+    zip_safe=False
 )
-
